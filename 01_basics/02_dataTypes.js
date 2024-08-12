8d4cba414cb68d86f5a86c390a1bd086d98cbfc2
Animesh3791 <90453065+Animesh3791@users.noreply.github.com>
2024-08-12 12:19:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3);
+
+console.log("Animesh")
+
+    let name = "Animesh"
+    let age = 18
+    let isLoggedIn = 'false'
+    
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined =>
+    // symbol => unique
+
+    //object
+    // null ois an object
+console.log(typeof undefined); // otput as undefined
+console.log(typeof null); // output as object
+
+
