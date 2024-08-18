ffc29f6ca499a09099e98eca14c14cad9dfed54d
Animesh3791 <90453065+Animesh3791@users.noreply.github.com>
2024-08-18 21:56:41 +0000

    Math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,53 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 123.8689
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//  +++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));  // convert only negative to positive
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(4));
+// console.log(Math.pow(4, 2));
+// console.log(Math.min(4, 3 , 8, 11));
+// console.log(Math.max(4, 3 , 8, 11));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
